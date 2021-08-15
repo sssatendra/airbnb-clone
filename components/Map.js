83b123e71234383fe48f2752570713b9731ca38b
sssatendra <74877594+sssatendra@users.noreply.github.com>
2021-08-15 21:52:08 +0530

    minor

--- components/Map.js
+++ components/Map.js
@@ -33,17 +33,16 @@ function Map({ searchResults }) {
             onViewportChange={(nextViewport) => setViewport(nextViewport)}
         >
             {searchResults.map((item) => (
-                <div key={item.long} className="relative z-15">
+                <div key={item.long} className="relative z-10">
                     <Marker longitude={item.long}
                         latitude={item.lat}
                         offsetLeft={-20}
                         offsetTop={-10}
-                        className="z-50"
                     >
-                        <p onClick={() => setSelectedLocation(item)} className="cursor-pointer text-2xl animate-bounce relative"
+                        <p onClick={() => setSelectedLocation(item)} className="cursor-pointer text-2xl animate-bounce"
                             aria-label="push-pin"
                             role="img"
-                        ><svg xmlns="http://www.w3.org/2000/svg" className="relative h-5 w-5 z-20 text-red-500" viewBox="0 0 20 20" fill="currentColor">
+                        ><svg xmlns="http://www.w3.org/2000/svg" className="z-10 h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                             </svg></p>
                     </Marker>
@@ -53,18 +52,15 @@ function Map({ searchResults }) {
                             closeOnClick={true}
                             latitude={item.lat}
                             longitude={item.long}
-                            className="z-20"
+                            className="z-50"
                         >
-                            <div className="bg-gradient-to-t from-gray-800 to-transparent rounded-2xl z-100 w-80 h-40 relative z-30">
-                                <Image loading="lazy" className="rounded-2xl bg-cover bg-gradient-to-t from-gray-800 to-transparent bottom-0 z-100" src={item.img} layout="fill" objectFit="cover" />
-                                <div
-                                    className=" bg-gradient-to-t from-gray-800 to-transparent bottom-0 w-full "
-                                >
-                                    <div className="absolute bg-gradient-to-t from-black to-transparent bottom-0 px-6">
-                                        <h5 className="text-white font-semibold">{item.title}</h5>
-                                        <h4 className="text-white font-semibold">{item.price} </h4>
-                                    </div>
+                            <div className="bg-gradient-to-t from-gray-800 to-transparent rounded-2xl w-80 h-40">
+                                <Image loading="lazy" className=" rounded-2xl bg-cover bg-gradient-to-t from-gray-800 to-transparent bottom-0 z-100" src={item.img} layout="fill" objectFit="cover" />
+                                <div className="absolute  bg-gradient-to-t from-black to-transparent bottom-0 px-6 py-3">
+                                    <h5 className="text-white font-semibold">{item.title}</h5>
+                                    <h4 className="text-white font-semibold">{item.price} </h4>
                                 </div>
+
                             </div>
 
                         </Popup>
